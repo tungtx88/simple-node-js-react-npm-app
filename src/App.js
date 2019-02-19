c843cd761e2d68ebe9e108aca4c940047784977e
tungtx88 <46309104+tungtx88@users.noreply.github.com>
2019-02-19 16:10:35 +0700

    Update App.js
    update tung tung

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React tung tung</h1>
         </header>
         <p className="App-intro">
           To get started, edit this <code>src/App.js</code> and save to reload.
