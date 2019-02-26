6cb0114bc18376856adae3fb8e536197220f0675
tungtx88 <46309104+tungtx88@users.noreply.github.com>
2019-02-26 13:57:19 +0700

    update
    update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React app</h1>
+          <h1 className="App-title">Welcome to React app 2</h1>
         </header>
         <p className="App-intro">
           test
